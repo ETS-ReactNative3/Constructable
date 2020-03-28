1a853839eb1a6b11df7b918a6c8b805ecd637848
Shaunak Bhandarkar <shaunakb@berkeley.edu>
2020-03-28 15:31:04 -0700

    Added addproject and newproject routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -8,6 +8,7 @@ app.config["MONGO_URI"] = "mongodb+srv://Shaunak:construction@constructable-6isx
 mongo = PyMongo(app)
 
 employee_id = None
+project_id = None
 
 @app.route("/getworker", methods = ["GET"])
 def get_worker():
@@ -22,6 +23,14 @@ def login():
     worker = mongo.db.Workers.find_one_or_404({"first_name": name})
     employee_id = worker['_id']
     return "Logged in as worker " + name
+
+@app.route("/selectproject")
+def select_project():
+    global project_id
+    name = request.args.get('name')
+    project = mongo.db.Workers.find_one_or_404({"name": name})
+    project_id = project['_id']
+    return "Selected project " + name
     
 
 @app.route("/gettasks", methods = ["GET"])
@@ -29,7 +38,7 @@ def worker_tasks():
     if (employee_id == None):
         return "No employee selected"
     worker = mongo.db.Workers.find_one_or_404({"_id": employee_id})
-    current_tasks = worker['tasks']['previous']
+    current_tasks = worker['tasks']['current']
     output = []
     for task in current_tasks:
         project = mongo.db.Projects.find_one_or_404({"_id": task['project_id']})
@@ -37,16 +46,33 @@ def worker_tasks():
         output.extend(task_full)
     return json_util.dumps(output)
 
+#add current worker to project specified
+@app.route("/addproject")
+def add_project():
+    if (employee_id == None):
+        return "No employee selected"
+    project_name = request.args.get('name')
+    project = mongo.db.Projects.find_one_or_404({"name": project_name})
+    project['workers'].append(employee_id)
+    mongo.db.Projects.update({"name": project_name}, project)
+    print("Added worker to " + project_name)
+
+@app.route("/newproject", methods = ["POST"])
+def new_project():
+    project = request.form.to_dict()
+    project['type'] = project['type'].split() #type entered as words with single space in between
+    project['workers'] = []
+    project['tasks'] = []
+    projectStr = str(project).replace("'", '"')
+    print("Added project: " + projectStr)
+    mongo.db.Projects.insert_one(json_util.loads(projectStr))
+    return "Project Added!"
+    
 
 @app.route("/addworker", methods = ["POST"])
 def add_worker():
     worker = request.form.to_dict()
-    worker['score'] = float(worker['score'])
     workerStr = str(worker).replace("'", '"')
     print("Added worker: " + workerStr)
     mongo.db.Workers.insert_one(json_util.loads(workerStr))
     return "Worker Added!"
-
-@app.route("/form")
-def show_form():
-    return render_template("newuser.html")
