6e81144fea3fd5c3fc8d1a6c8f01a9ce49f056de
Shaunak Bhandarkar <shaunakb@berkeley.edu>
2020-03-28 00:28:53 -0700

    Get by name

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -12,7 +12,10 @@ def hello():
 def main():
     return "this is the main page"
 
-@app.route("/get")
-def get_score():
-    score = mongo.db.Workers.find({"name": "Bob"})
-    return str(score[0]["score"])
+@app.route("/get/<name>")
+def get_score(name):
+    result = 0 # default value
+    score = mongo.db.Workers.find({"name": name})
+    for s in score:
+        result = s['score']
+    return str(result)
