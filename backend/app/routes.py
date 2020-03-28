a0a08e39df51ee58f7052962f12cdae7139796ad
Shaunak Bhandarkar <shaunakb@berkeley.edu>
2020-03-28 14:18:50 -0700

    Added login and gettasks methods

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,16 +4,39 @@ from flask import jsonify, request, url_for, render_template
 from bson import json_util
 import bson
 
-app.config["MONGO_URI"] = "mongodb+srv://Shaunak:construction@constructable-6isx0.mongodb.net/Users"
+app.config["MONGO_URI"] = "mongodb+srv://Shaunak:construction@constructable-6isx0.mongodb.net/AppData"
 mongo = PyMongo(app)
 
-employee_id = 0
+employee_id = None
 
 @app.route("/getworker", methods = ["GET"])
 def get_worker():
     name = request.args.get('first_name')
     worker = mongo.db.Workers.find_one_or_404({"first_name": name})
     return json_util.dumps(worker)
+
+@app.route("/login")
+def login():
+    global employee_id
+    name = request.args.get('first_name')
+    worker = mongo.db.Workers.find_one_or_404({"first_name": name})
+    employee_id = worker['_id']
+    return "Logged in as worker " + name
+    
+
+@app.route("/gettasks", methods = ["GET"])
+def worker_tasks():
+    if (employee_id == None):
+        return "No employee selected"
+    worker = mongo.db.Workers.find_one_or_404({"_id": employee_id})
+    current_tasks = worker['tasks']['previous']
+    output = []
+    for task in current_tasks:
+        project = mongo.db.Projects.find_one_or_404({"_id": task['project_id']})
+        task_full = [t for t in project['tasks'] if t['task_id'] == task['task_id']]
+        output.extend(task_full)
+    return json_util.dumps(output)
+
 
 @app.route("/addworker", methods = ["POST"])
 def add_worker():
@@ -23,7 +46,6 @@ def add_worker():
     print("Added worker: " + workerStr)
     mongo.db.Workers.insert_one(json_util.loads(workerStr))
     return "Worker Added!"
-
 
 @app.route("/form")
 def show_form():
