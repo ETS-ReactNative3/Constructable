c1c71e4eb2b672670d93b3f0880bc0943a7cce87
Shaunak Bhandarkar <shaunakb@berkeley.edu>
2020-03-28 11:48:02 -0700

    Changed to match data

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,22 +2,26 @@ from app import app
 from flask_pymongo import PyMongo
 from flask import jsonify, request, url_for, render_template
 from bson import json_util
+import bson
 
 app.config["MONGO_URI"] = "mongodb+srv://Shaunak:construction@constructable-6isx0.mongodb.net/Users"
 mongo = PyMongo(app)
 
-@app.route("/getworker/<name>", methods = ["GET"])
-def get_worker(name):
-    worker = mongo.db.Workers.find({"name": name})
-    worker_data = [d for d in worker]
-    return json_util.dumps(worker_data)
+employee_id = 0
+
+@app.route("/getworker", methods = ["GET"])
+def get_worker():
+    name = request.args.get('first_name')
+    worker = mongo.db.Workers.find_one_or_404({"first_name": name})
+    return json_util.dumps(worker)
 
 @app.route("/addworker", methods = ["POST"])
 def add_worker():
     worker = request.form.to_dict()
     worker['score'] = float(worker['score'])
-    print("Added worker: " + worker)
-    mongo.db.Workers.insert_one(request.form.to_dict())
+    workerStr = str(worker).replace("'", '"')
+    print("Added worker: " + workerStr)
+    mongo.db.Workers.insert_one(json_util.loads(workerStr))
     return "Worker Added!"
 
 
