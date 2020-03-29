487c25f8efee53c9b4943f27bde43f692b86bad7
Shaunak Bhandarkar <shaunakb@berkeley.edu>
2020-03-28 23:17:24 -0700

    Finished more routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -53,7 +53,7 @@ def worker_projects():
     #else if role is worker
     current_tasks = worker['tasks']['current']
     output = []
-    project_ids = list(set([task['project_id' for task in current_tasks])))
+    project_ids = list(set([task['project_id'] for task in current_tasks]))
     for pid in project_ids:
         project = mongo.db.Projects.find_one_or_404({"_id": pid})
         output.append(project)
@@ -87,6 +87,7 @@ def new_project():
     project['type'] = project['type'].split(',') #type entered as words with commas in between
     project['workers'] = []
     project['tasks'] = []
+    project['completion time'] = int(project['completion time'])
     projectStr = str(project).replace("'", '"')
     new_project = json_util.loads(projectStr)
     new_project['supervisor'] = supervisor_id
@@ -150,10 +151,33 @@ def new_post():
     post['likes'] = 0
     post['views'] = 0
     post['comments'] = []
-    post['timestamp'] = datetime.datetime.now()
     postStr = str(post).replace("'", '"')
     print("Added post: " + postStr)
     new_post = json_util.loads(postStr)
     new_post['user_id'] = employee_id
+    if (len(list(request.files.to_dict().values())) != 0):
+        file = list(request.files.to_dict().values())[0]
+        pic_id = fs.put(file, content_type=file.content_type, filename=file.filename)
+        new_post['pic_id'] = pic_id
     mongo.db.Posts.insert_one(new_post)
     return "Post added!"
+
+#get picture by post id
+@app.route("/getpostpic", methods = ["GET"])
+def get_post_pic():
+    post_id = bson.objectid.ObjectId(request.args.get('post_id'))
+    post = mongo.db.Posts.find_one_or_404({"_id": post_id})
+    pic_id = post['pic_id']
+    file = fs.get(pic_id)
+    response = make_response(file.read())
+    response.mimetype = file.content_type
+    return response
+
+#returns all posts if no id specified, one worker's posts if id is specified
+@app.route("/getpostdata", methods = ["GET"])
+def all_post_data():
+    if (request.args.get('employee_id') == None):
+        return json_util.dumps(mongo.db.Posts.find())
+    employee_id = bson.objectid.ObjectId(request.args.get('employee_id'))
+    all_posts = mongo.db.Posts.find({"user_id": employee_id})
+    return json_util.dumps(all_posts)
